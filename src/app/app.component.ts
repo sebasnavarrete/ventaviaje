f130d9f03a1850eca4a9fb13f770ee1a4c638c7a
Sebastian Navarrete <sebasnavarrete.2@gmail.com>
2023-05-23 13:52:05 -0500

    products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -88,6 +88,60 @@ export class AppComponent {
       price: 1000000,
       images: ["ipad.jpeg", "ipad2.jpeg"],
       sold: false
+    },{
+      title: "Filtro Renaware Aqua Nano CTU-500 HD",
+      subtitle: `Excelente estado`,
+      price: 2700000,
+      images: ["filtro.jpeg"],
+      sold: false
+    },{
+      title: "Impresora EPSON",
+      subtitle: `M3170`,
+      price: 1500000,
+      images: ["impresora.jpeg"],
+      sold: false
+    },{
+      title: "Lavadora/Secadora LG",
+      subtitle: `22/13kg`,
+      price: 4000000,
+      images: ["lavadora.jpeg"],
+      sold: false
+    },{
+      title: "Pantalla Samsung",
+      subtitle: `27"`,
+      price: 500000,
+      images: ["pantallas.jpeg"],
+      sold: false
+    },{
+      title: "Puff redondo",
+      subtitle: `Diametro: `,
+      price: 900000,
+      images: ["puffred.jpeg"],
+      sold: false
+    },{
+      title: "Silla gamer blanca",
+      subtitle: `Con masajeador de espalda y luces led`,
+      price: 450000,
+      images: ["sillab.jpeg"],
+      sold: false
+    },{
+      title: "Calentador Kalley",
+      subtitle: `K-CAT`,
+      price: 150000,
+      images: ["calentador.jpeg"],
+      sold: false
+    },{
+      title: "VideoBeam EPSON Powerlite L200SW",
+      subtitle: `Excelente estado - Sin caja`,
+      price: 6500000,
+      images: ["videob.jpeg"],
+      sold: false
+    },{
+      title: "Monitor Gamer LG 27gl850-b 27 Nanoips",
+      subtitle: `Excelente estado`,
+      price: 1400000,
+      images: ["pantallalg.jpeg","pantallalg2.png"],
+      sold: false
     }
   ];
 }
